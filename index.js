d17c20ce05b063c439db8e1720509e8d29be304f
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-14 13:30:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
   
   var recipes = {}
   
-  // 2) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value 
+  // 2) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value 
+  function updateObjectWithKeyAndValue(object, key, value){
+    Object.assign({},object,{[key]:value})
+  }
+  
   function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
     // object[key]=[value]
 
